1ec1a4bb2365baca71f7ed86886722a4ce16122c
Ahan Gupta <ahangupta.96@gmail.com>
2018-02-17 10:44:42 +0800

    Created the event Controller, without implmentation of CRUD methods

---
+++ myapp/controllers/eventController.js
@@ -0,0 +1,17 @@
+var event = require('../models/todoEventModel.js');
+
+exports.create_todoEvent = function(req, res, next) {
+
+};
+
+exports.read_todoEvent = function(req, res, next) {
+
+};
+
+exports.update_todoEvent = function(req, res, next) {
+
+};
+
+export.delete_todoEvent = function(req, res, next) {
+
+};
