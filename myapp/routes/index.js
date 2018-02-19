0e44c4c09ef6b0c8f1f6750917bd538e69138df0
Ahan Gupta <ahangupta.96@gmail.com>
2018-02-19 14:18:10 +0800

    Creation of an empty delete page and relevant routes to handle delete requests

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,7 @@
 // The route handling the main page
 var express = require('express');
 var router = express.Router();
+var eventController = require('../controllers/eventController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
